4bae4f7d415c1cce7990ef9e8a8127310c092a95
tHeMaskedMan981 <akash.minato@gmail.com>
2019-08-04 06:44:59 +0530

    some cleaning done

--- client/src/components/Pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PageFAQs } from './FAQs';
-export { default as PageDesktop } from './Desktop/Desktop';
-export { default as PageMetaMaskLogIn } from './MetaMaskLogIn/MetaMaskLogIn';
-export { default as PageMetaMaskMissing } from './MetaMaskMissing/MetaMaskMissing';
-export { default as PageNotFound } from './NotFound/NotFound';
--- client/src/utilities/index.js
+++
@@ -1,1 +0,0 @@
-export { promisify } from './promisify'
