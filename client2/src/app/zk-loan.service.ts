9f59ea45fc2c8e2abd7eddccff66633683016e84
Tezan <tezansahu@gmail.com>
2019-08-04 08:40:47 +0530

    integration in progress

--- client2/src/app/zk-loan.service.ts
+++ client2/src/app/zk-loan.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 import {Web3Service} from './util/web3.service';
 import kernel from "../artifacts/Kernel.json";
 import zkLoan from "../artifacts/ZkLoan.json";
+import dai from  "../artifacts/DAI.json";
+import weth from "../artifacts/WETH.json";
+import joinsplit from "../artifacts/JoinSplit.json"
+import ace from "../artifacts/ACE.json"
+
 import { HttpClient } from '@angular/common/http';
+import { note, JoinSplitProof } from "aztec.js";
 // import {EthCrypto} from 'eth-crypto';
 
 @Injectable({
@@ -12,15 +18,32 @@ export class ZkLoanService {
 
   private kernelContract: any;
   private deployedKernelContract: any;
+  private deployedDAIContract: any;
+  private deployedWETHContract: any;
+  private deployedJoinSplitContract: any;
+  private deployedACEContract: any;
 
 
   constructor(private web3Service: Web3Service, private http: HttpClient) { 
-    this.getKernelContract();
+    this.getContracts();
   }
 
-  private async getKernelContract(){
+  private async getContracts(){
     this.kernelContract = await this.web3Service.artifactsToContract(kernel);
     this.deployedKernelContract = await this.kernelContract.deployed();
+
+    let DAIContract = await this.web3Service.artifactsToContract(dai);
+    this.deployedDAIContract = await dai.deployed();
+
+    let WETHContract = await this.web3Service.artifactsToContract(weth);
+    this.deployedWETHContract = await weth.deployed();
+
+    let joinsplitContract = await this.web3Service.artifactsToContract(joinsplit);
+    this.deployedJoinSplitContract = await joinsplitContract.deployed();
+
+    let aceContract = await this.web3Service.artifactsToContract(ace);
+    this.deployedACEContract = await aceContract.deployed();
+
   }
 
   public async requestViewAccess(loanHash: string, address: string){
@@ -61,7 +84,7 @@ export class ZkLoanService {
        try{ 
         let url = "http://localhost:8000/kernel/viewKey/" + loanHash;
           this.http.get(url).subscribe(async (res) => {
-          let viewKey = res["view_key"];
+          let viewKey = res["result"]["view_key"];
           let pubKey = this.addressToPubKey(address);
 
           let sharedSecret = this.encrypt(viewKey, pubKey);
@@ -74,5 +97,47 @@ export class ZkLoanService {
         resolve(false);
       }
     })
+  }
+
+  public async createKernel(json_body){
+    return new Promise(async resolve => {
+      // await protocolTokenInstance.approve(ZkLoanInstance.address, 1, {from: json_body["lender"]})
+
+      // let depositInputNotes1 = [];
+      // let depositOutputNotes1 = [await note.create(this.addressToPubKey(json_body["lender"]), json_body["loan_amount"])]
+      // let depositPublicValue1 = json_body["loan_amount"] * -1;
+      // let depositInputOwnerAccounts1 = [];
+
+      // const depositProof1 = new JoinSplitProof(depositInputNotes1, depositOutputNotes1, json_body["lender"], depositPublicValue1, json_body["lender"]);
+      // const depositData1 = depositProof1.encodeABI(json_body["lend_currency_address"]);
+      // const depositSignatures1 = depositProof1.constructSignatures(json_body["lend_currency_address"], depositInputOwnerAccounts1);
+
+      // await ERC20_AInstance.approve(ACEInstance.address, -depositPublicValue1, {from: json_body["lender"]});
+      let dummyPublicKey = "0x047c7b4dfedccb80aa11132e4b5411e96d9fc4057e1cb74a8058ca003fc707473f34d40713927ebdd721bd4ac4f7bef50456a5eff02bc888127c40e2c67eeda823";
+      let salt = "0x7bf20bc9c53493cfd19f9378b1bb9f36ceeee7e76b724efeca38f7d1c96f8a04";
+
+      let lendCurrencyNote = note.create(this.addressToPubKey(json_body["lender"]), json_body["loan_amount"]);
+      let borrowCurrencyNote = note.create(dummyPublicKey, json_body["collateral_amount"]);
+
+
+      let kernelHash = await this.deployedKernelContract.kernel_hash(
+          [json_body["lender"], json_body["borrower"], json_body["relayer"], json_body["wrangler"], json_body["borrow_currency_address"], json_body["lend_currency_address"]],
+          [json_body["monitoring_fee"], json_body["expires_at"], json_body["daily_interest_rate"], json_body["position_duration"]],
+          [lendCurrencyNote.noteHash, borrowCurrencyNote.noteHash, salt]
+      )
+
+      json_body["kernel_hash"] = kernelHash;
+      json_body["lend_currency_noteHash"] = lendCurrencyNote.noteHash;
+      json_body["borrow_currency_noteHahs"] = borrowCurrencyNote.noteHash;
+
+      const url = "http://localhost:8000/kernel";
+      this.http.post(url, json_body).subscribe(async(res) => {
+        console.log(res);
+      })
+
+      resolve(true);
+
+
+    });
   }
 }
---
+++ contracts/zkDAI.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.7;
+
+import "./AZTEC/ERC1724/ZkAsset.sol";
+
+contract ZkDAI is ZkAsset{
+
+    event Log(address owner, uint256 value, bytes32 noteHash);
+    // event LogString(string value);
+
+    address public aceAddress;
+    address public erc20Address;
+
+    constructor(
+        address _aceAddress,
+        address _erc20Address
+    ) public ZkAsset(_aceAddress, address(_erc20Address), 1) {
+        aceAddress = _aceAddress;
+        erc20Address = _erc20Address;
+    }
+
+    function approveAceToSpendTokens(bytes32 _proofHash, uint256 _value) public {
+        ACE aceContract = ACE(aceAddress);
+        aceContract.publicApprove(address(this), _proofHash, _value);
+        emit Log(aceAddress, _value, _proofHash);
+    }
+
+    function getACE() external view returns(address){
+        return aceAddress;
+    }
+
+}
